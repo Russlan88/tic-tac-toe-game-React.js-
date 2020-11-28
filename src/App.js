44137f57acb1c7b7ff145b4dee1d678f926a3a79
Ruslan <ruslan.ogarkov88@gmail.com>
2020-11-28 15:16:39 +0200

    removed console log from app js

--- src/App.js
+++ src/App.js
@@ -15,8 +15,6 @@ const App = () => {
 
   const current = history[currentMove];
 
-  console.log('history', history)
-
   const winner = calculateWinner(current.board);
 
   const message = winner ? `Winner is ${winner}` : `Next player is ${current.isXNext ? 'X' : '0'}`;
